4d842db95f110c32f7af929b783ae5d3b78c3df5
Kristin Barker <kristinjbarker@gmail.com>
2015-11-25 14:38:30 -0700

    Completed power analysis! But not positive I'm using the right values, and still need to loop it to look at different data options.

--- bootstrap.R
+++ bootstrap.R
@@ -1,9 +1,9 @@
 setwd("C:/Users/kjbark3r/Documents/GitHub/RDlab")
 
-#read in data
+#data
 data2014 <- read.csv("data2014.csv")
 
-#call needed libraries
+#libraries
 library(dplyr)
 
 ############################################################
@@ -16,18 +16,19 @@ mig.data2014 <- filter(data2014, MigStatus == "Mig")
 mig.bootdata <- sample(1:nrow(mig.data2014), 24, replace = T) 
 (mig.newdata <- mig.data2014[mig.bootdata,])
 
-##set up vector to store migrant means in
+##set up vector to store migrant averages in
 mig.finaldata <- NULL
 
 ##run loop 12 times, each time taking the average of 2 sequential data points
 for(i in 1:12) {
   mig.finaldata[i] <- sample(mig.newdata$PctFN, 2)
   }
-mig <- data.frame(mig.finaldata)
 
-##calculate migrant mean and variance
-mig.mean <- mean(mig$mig.finaldata)
-mig.var <- var(mig$mig.finaldata)
+#create properly formatted dataframe of bootstrapped migrant data
+mig <- data.frame(mig.finaldata)
+mig$mig.status <- "Migrant"
+colnames(mig)[1] <- "FecalN"
+
 
 ############
 #RESIDENTS - subset and bootstrap 
@@ -42,43 +43,40 @@ res.finaldata <- NULL
 for(i in 1:12) {
   res.finaldata[i] <- sample(res.newdata$PctFN, 2)
 }
-res <- data.frame(res.finaldata)
 
-##calculate resident mean and variance
-res.mean <- mean(res$res.finaldata)
-res.var <- var(res$res.finaldata)
+
+#create properly formatted dataframe of bootstrapped resident data
+res <- data.frame(res.finaldata)
+res$mig.status <- "Resident"
+colnames(res)[1] <- "FecalN"
+head(res)
+
 
 ############################################################
 # Combine data and run power analysis  
 ############################################################
 
-migres <- data.frame(c(mig, res))
+#combine migrant and resident data in tidy format
+fn <- bind_rows(mig, res)
 
-#determine variance to use in power analysis
-lm <- lm(mig.finaldata ~ res.finaldata, data = migres)
-summary(lm)
+#check out the data
+boxplot(FecalN ~ mig.status, fn, ylab  = "Fecal N (% dry matter)")
 
-anova(lm)
+#determine variance values to use in power analysis
+(lm <- summary(lm(FecalN ~ mig.status, data = fn)))
+
+
+#another (better) way?
+(aov <- summary(aov(FecalN ~ mig.status, data = fn)))
 
 
 #power analysis 
-##first, determine what my power is given my current sample size
-##then, play with parameters
-power.anova.test(groups = 2, n = 12, between.var = NULL, 
-                 within.var = XXXXX)
-
-
-############################################################
-# Just playing around with data manipulation
-# not for this project, just for future/general knowledge
-############################################################
-#trying some things I found on teh interwebz
-
-##create data frame with two columns - mig FN and res FN
-##in future do this and add sampling period as 3rd column
-##so you can plot by sampling pd, look at temporal changes, etc
-migres <- data.frame(c(mig, res))
-
-##put all FN values from above into single vector
-migres.df <- as.vector(c(migres$mig.finaldata, migres$res.finaldata))
+##First, determine what my power is given my current sample size.
+##If not enough power to detect 5% effect size, 
+##figure out what effect size I can detect
+###Notes to self
+####between.var is variance of group means
+####within.var is MSE from ANOVA
+power.anova.test(groups = 2, n = 12, between.var = 0.4312, within.var = 0.1726,
+                 sig.level = 0.05)
 
